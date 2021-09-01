eca83bfbabd7a30ddc52b09b2e07ca955f10b59a
Lucas Peres <lucasperesss@gmail.com>
2021-09-01 18:31:33 -0300

    Requisito 8 simplificado

--- src/myCounter.js
+++ src/myCounter.js
@@ -15,15 +15,6 @@
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
 
-const myCounter = () => {
-  var myArray = [];
-  for (let counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
-      myArray.push(counter2);
-    }
-  }
-  return myArray;
-};
+const myCounter = () => [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 
 module.exports = myCounter;
