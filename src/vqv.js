4743be93d3d614058a7af8b33b7a2926877d02fd
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-01 17:09:43 -0300

    Finaliza requisito 03.

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if ((nome || idade) === undefined) return undefined;
+  return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+  // console.log(`Oi, meu nome é ${nome}!
+  // Tenho ${idade} anos,
+  // trabalho na Trybe e mando muito em programação!
+  // #VQV!`);
+};
 
 module.exports = vqv;
+
+// console.log( vqv('Daniel Dias', 22) );
