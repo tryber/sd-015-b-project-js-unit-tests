2b872b2c94fd208bd67c34f41708a38a08f385f4
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2021-09-01 15:00:14 -0300

    requesitos 1 e lint ok

--- src/average.js
+++ src/average.js
@@ -12,9 +12,22 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // add your implementation here
+const average = (array) => {
+  let sum = 0;
+  let result;
 
+  if (array.length === 0) {
+    return undefined;
+  }
+
+  for (let i = 0; i < array.length; i += 1) { 
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    }
+    sum += array[i];
+  }
+  result = sum / array.length;
+  return Math.round(result);
 };
 
 module.exports = average;
