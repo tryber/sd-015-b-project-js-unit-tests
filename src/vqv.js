b7ec5e77ccb924dcea592832ce1de5f8826a557e
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-09-01 16:09:10 -0300

    Correção requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,9 +16,15 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => `Oi, meu nome é ${nome}!
-Tenho ${idade} anos,
-trabalho na Trybe e mando muito em programação!
-#VQV!`;
+const vqv = (nome, idade) => {
+  if (nome === undefined && idade === undefined) {
+    return undefined;
+  }
+  return `Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`;
+};
 
+console.log(vqv());
 module.exports = vqv;
