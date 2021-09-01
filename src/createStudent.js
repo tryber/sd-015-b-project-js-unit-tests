c774a98146dfa56a12f52bbcc7e1a33c3ae71de6
Antônio Campos <aclcampos@live.com>
2021-09-01 19:38:11 -0300

    Realizado requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,8 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => ({ name: nome, feedback: () => 'Eita pessoa boa!' });
+
+console.log(typeof createStudent('Antonio').feedback);
 
 module.exports = createStudent;
