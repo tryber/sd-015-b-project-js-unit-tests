c1327df8d4601d6afc7198aeb91982f93a8a5665
gustavogss <gustavogss.dev@gmail.com>
2021-09-01 18:46:42 -0300

    Requisito 8 -erro de lint

--- src/myCounter.js
+++ src/myCounter.js
@@ -20,7 +20,7 @@ const myCounter = () => {
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (var counter2 = 2; counter2 <= 3; counter2 += 1) {
-      myArray.push(counter2); //deepStrictEqual
+      myArray.push(counter2); 
     }
   }
   return myArray;
