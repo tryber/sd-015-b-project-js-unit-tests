b2b1f59e60f170b2ead9ee29cd09f17df900f6e8
PedroAugmed <pedroaugmed7@gmail.com>
2021-09-01 15:51:13 -0300

    Add Requisito 1

--- src/average.js
+++ src/average.js
@@ -1,3 +1,5 @@
+/* eslint-disable complexity */
+/* eslint-disable no-unused-expressions */
 /*
   A função average recebe um array (tamanho variável) e retorna a média dos valores recebidos.
   Caso a função receba algum valor não númerico ou um array vazio,
@@ -12,6 +14,29 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+// eslint-disable-next-line sonarjs/cognitive-complexity
+const average = (a) => {
+const array = a;
+if (array.length === 0) {
+return undefined;
+}
+let soma = 0;
+let arrayNumericoNãoVazio = true;
+for (let i = 0; i < array.length; i += 1) {
+    soma += array[i];
+if (typeof a[i] !== 'number') {
+arrayNumericoNãoVazio = false;
+}  
+}
+const media = soma / array.length;
+const mediaArredonada = Math.round(media);
+if (arrayNumericoNãoVazio === true) {
+return mediaArredonada;
+} 
+if (arrayNumericoNãoVazio === false) {
+return undefined;
+}
+};
 
+console.log(average([1, 1]));
 module.exports = average;
