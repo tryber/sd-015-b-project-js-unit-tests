e93426329343c6ac58fcbf29391cb5786a3e4e32
Gabriel Peralta <prates_peralta@hotmail.com>
2021-09-01 15:46:21 -0300

    corrige problema de aspas duplas no lint

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -1,8 +1,8 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
-const assert = require('assert");
-const numbers = require("../src/numbers");
+const assert = require('assert');
+const numbers = require('../src/numbers');
 
 /*
   A função numbers recebe um array (tamanho variável) e retorna true se todos os parâmetros forem do tipo 'number' e false caso contrário.
@@ -16,8 +16,8 @@ const numbers = require("../src/numbers");
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe("2 - Implemente os casos de teste para a função `numbers`", () => {
-  it("Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros e falso caso contrário", () => {
+describe('2 - Implemente os casos de teste para a função `numbers`', () => {
+  it('Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros e falso caso contrário', () => {
     // assert.fail();
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
 
