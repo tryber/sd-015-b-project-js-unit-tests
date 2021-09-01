141965f472a4662f093d75b2416ac11a78921df2
Gabriel Jorge <gabrieljorge.ca@gmail.com>
2021-09-01 16:59:25 -0300

    Adiciona requisito 03 e corrige lint

--- src/vqv.js
+++ src/vqv.js
@@ -17,17 +17,15 @@
 */
 
 const vqv = (nome, idade) => {
-  if(nome !== undefined && idade !== undefined) {
-  return `Oi, meu nome é ${nome}!
+  if (nome !== undefined && idade !== undefined) {
+    return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV!`
-  } else {
-      return undefined
-    }
-}
-  
+#VQV!`;
+  }
+    return undefined;
+};
 
-console.log(vqv('Tunico',29))
+console.log(vqv('Tunico', 29));
 
 module.exports = vqv;
