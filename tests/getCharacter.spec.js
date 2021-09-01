c7a1a7aa594de734020dcd01a33c1d992373e2a9
matheusg18 <matheusgabriel.18@gmail.com>
2021-09-01 17:17:34 -0300

    Requisito 8 feito

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -46,8 +46,8 @@ OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enq
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    assert.fail();
-    // ESCREVA SEUS TESTES ABAIXO:
+    assert.strictEqual(typeof getCharacter(), 'undefined');
+
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
