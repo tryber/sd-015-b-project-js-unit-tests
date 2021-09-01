4706e0bf17d218ece807526131f78058306d876c
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-01 16:18:21 -0300

    Corrige erro de estrutura do codigo.

--- src/average.js
+++ src/average.js
@@ -17,7 +17,7 @@ const average = (array) => {
   let valor = 0;
   for (let index = 0; index < array.length; index = index+1) {
     // Captura todos os valores depois retorna o total dividido pelo array.lenght
-    if (typeof array[index] === 'number')) {
+    if (typeof array[index] === 'number') {
       valor += array[index];
     } else {
       return undefined;
