50438b5481a31fee50485c8bc9c5dff70fa4163a
Wander Luiz <wander.srv@gmail.com>
2021-09-01 15:25:09 -0300

    Resoluçao do requisito 1

--- src/average.js
+++ src/average.js
@@ -12,7 +12,23 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let media;
+  let soma = 0;
+  
+  if (array.length === 0) {
+    return undefined;
+  }
+
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    soma += array[index];
+  }
+  media = soma / array.length;
+
+  return Math.round(media);
+};
 
 module.exports = average;
-
