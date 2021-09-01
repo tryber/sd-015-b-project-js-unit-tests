912175534c39fb4d42bee4a04fe9c6fc6f3797b6
Virginia <vihvasc@gmail.com>
2021-09-01 14:58:30 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -12,7 +12,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-
-const average = () => {};
+const average = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
+  let total = 0;
+  let resultado = 0;
+    for (let index = 0; index < array.length; index += 1) {
+      if (typeof array[index] !== 'number') {
+        return undefined;
+      }
+        total += array[index];
+    }
+    resultado = total / array.length;
+    return Math.round(resultado);
+};
 
 module.exports = average;
