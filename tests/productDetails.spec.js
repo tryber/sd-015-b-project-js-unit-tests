09a9459e21a5f87c794e24b0fa4e99a6fa947624
DanielHott <daniel_hott@outlook.com>
2021-09-01 17:39:19 -0300

    Adicionando sexto requisito

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const { type } = require('os');
 const productDetails = require('../src/productDetails');
 
 /*
@@ -30,12 +31,25 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    const entrada1 = productDetails('Alcool gel', 'Máscara');
+    const tipo1 = typeof entrada1;
+    assert.deepStrictEqual(tipo1, "object", 'Não é um array');
     // Teste que o array retornado pela função contém dois itens dentro.
+    const entrada2 = productDetails('Alcool gel', 'Máscara');
+    const tamanho = entrada2.length;
+    assert.deepStrictEqual(tamanho, 2, 'Tamanho inválido retornado');
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    const entrada3 = productDetails('Alcool gel', 'Máscara');
+    const item1 = entrada3[0];
+    const tipo = typeof item1;
+    const item2 = entrada3[1];
+    const tipo2 = typeof item2;
+    assert.deepStrictEqual(tipo, "object", 'O primeiro item retornado não é um array');
+    assert.deepStrictEqual(tipo2, "object", 'O segundo item retornado não é um array!');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepEqual(item1, item2, 'Os itens são iguais!');
     // Teste que os dois productIds terminam com 123.
   });
 });
