5be50fb0358f47f736173f85d706d58445a7d830
Amanda Fernandes <aferanda@gmail.com>
2021-09-01 19:18:29 -0300

    Requisito 6

--- src/productDetails.js
+++ src/productDetails.js
@@ -39,4 +39,6 @@ const productDetails = (firstProduct, secondProduct) => [
   },
 ];
 
+console.log(productDetails('Alcool gel', 'Máscara')[0].details.productId.endsWith('1234'));
+
 module.exports = productDetails;
