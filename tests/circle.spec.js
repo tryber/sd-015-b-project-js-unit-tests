46fc6f4c7ee2bf536354dbb0bab4a539c1508fe1
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-09-01 18:17:21 -0300

    Requisito 4, teste com dicas do Isaac

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -22,11 +22,14 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
+    const objectParsed = parseFloat((Object.values(circle(2))[2]).toFixed(2));
+    const objectParsed2 = parseFloat((Object.values(circle(3))[1]).toFixed(2));
+
     assert.strictEqual(typeof circle(2), 'object');
     assert.strictEqual(Object.keys(circle(2)).length, 3);
     assert.strictEqual(circle(), undefined);
-    assert.strictEqual(Object.values(circle(2))[2], 12.56);
-    assert.strictEqual(Object.values(circle(3))[1], 28.26);
-    assert.deepStrictEqual(circle(3), { radius: 3, area: 28.26, circumference: 18.84 });
+    assert.strictEqual(objectParsed, 12.56);
+    assert.strictEqual(objectParsed2, 28.26);
+    assert.deepStrictEqual(circle(3), { radius: 3, area: 28.259999999999998, circumference: 18.84 });
   });
 });
