392594ace2eeff6bf88532d471f3f57a7e442301
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-09-01 19:40:23 -0300

    implementa os casos de teste na funcao getCharacter para o desafio 9

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -46,13 +46,23 @@ OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enq
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(getCharacter(), undefined);
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    assert.strictEqual(typeof getCharacter('Arya'), 'object');
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    assert.strictEqual(typeof getCharacter('Brienne'), 'object');
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    assert.strictEqual(typeof getCharacter('Melissandre'), 'object');
     // Teste se a função se os parâmetros não são Case Sensitive.
+    const recieved = getCharacter('ArYA');
+    const expected = getCharacter(recieved.toLowerCase)
+    assert.notStrictEqual(recieved, expected);
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    const recieved2 = getCharacter('ROdolFO');
+    const expected2 = undefined;
+    assert.strictEqual(recieved2, expected2);
   });
 });
