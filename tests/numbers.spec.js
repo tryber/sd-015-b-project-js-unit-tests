2b2c057c9084474ba3f97796ac3d5dfb3389d729
Gabriel Jorge <gabrieljorge.ca@gmail.com>
2021-09-01 16:21:36 -0300

    Adiciona requisito 02 teste 01

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -18,7 +18,8 @@ const numbers = require('../src/numbers');
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros e falso caso contrário', () => {
-    assert.fail();
+    const x =  numbers([1,2,3,4,5])
+    assert.strictEqual(x, true);
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
