fd5d529684c52667e90bec2b6d2a572c08c6a15c
fernandamichetti <f.michetti@hotmail.com>
2021-09-01 15:33:11 -0300

    Primeiro requisito completado

--- src/average.js
+++ src/average.js
@@ -12,8 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // implemention goes here
+const average = (arr) => { 
+ let sum = 0;
+ if (arr.length === 0) {
+   return undefined;
+ }
+ for (let i = 0; i < arr.length; i += 1) {
+   if (typeof arr[i] !== 'number') {
+     return undefined;
+   }
+   sum += arr[i];
+ }
+ let result = sum / arr.length;
+ return Math.round(result);
 };
-
 module.exports = average;
