b5b620e5c3c63f616efcd2d4cd5a3e6519338abf
alvesgf16 <alvesgabriel1994@gmail.com>
2021-09-01 19:39:35 -0300

    Implementa parcialmente testes e funçao createMenu

--- src/restaurant.js
+++ src/restaurant.js
@@ -79,6 +79,16 @@
 // soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, 
 // você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = () => {};
+const createMenu = (objetoPassadoPorParametro) => {
+  const objetoRetornado = {
+    fetchMenu: () => objetoPassadoPorParametro,
+    consumption: [],
+    order: (item) => {
+      objetoRetornado.consumption.push(item);
+    },
+  };
+
+  return objetoRetornado;
+};
 
 module.exports = createMenu;
