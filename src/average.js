3270ce5a98f2dd1e30d8b2abd9b8fd25cc3ecf52
Danielen Cestari Nunes <danielen.cestari@gmail.com>
2021-09-01 15:39:16 -0300

    requisito 01 passando no teste

--- src/average.js
+++ src/average.js
@@ -24,7 +24,7 @@ const average = (array) => {
     } 
     soma += numeros;
   }
-  const resultado = Math.floor(soma / tamanho);
+  const resultado = Math.round(soma / tamanho);
   return resultado;
 };
 
