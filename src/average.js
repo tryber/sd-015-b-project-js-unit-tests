4a43d48529f71482aac815948bc4f367e48f6f9d
Vitor <vitoremerich.contato@gmail.com>
2021-09-01 16:39:48 -0300

    Iniciando projeto

--- src/average.js
+++ src/average.js
@@ -12,6 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
-
+const average = (array) => {
+  let sum = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    }
+    sum += array[i];
+  }
+  if (array.length === 0) {
+    return undefined;
+  }
+  const averageResult = Math.round(sum / array.length);
+  return averageResult;
+};
 module.exports = average;
