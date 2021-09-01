92b949b0782b120773142046d8da531a3e744612
Gabriel Jorge <gabrieljorge.ca@gmail.com>
2021-09-01 15:53:02 -0300

    Adiciona requisito 01

--- src/average.js
+++ src/average.js
@@ -12,6 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = (array) => {};
-
+const average = (array) => {
+  let sum = 0;
+  let numAverage = 0;
+  if (array.length === 0) {return undefined}
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    }
+    sum += array[i];
+    numAverage = sum / array.length;
+  }
+  return Math.round(numAverage);
+};
+const a = [];
+average(a);
 module.exports = average;
