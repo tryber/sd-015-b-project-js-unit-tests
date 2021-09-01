17c3a480366c15efe78c81164e878649d37c7de3
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2021-09-01 15:42:54 -0300

    inicia projeto e resolve requisito 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
+  const divisor = array.length;
+  let dividendo = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    dividendo += array[index];
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+  }
+  const media = Math.round(dividendo / divisor);
+  return media;
+};
 
 module.exports = average;
