eaadc022cdba0761f8df8a49da41e79c954f009d
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-01 16:15:08 -0300

    Resolve requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,14 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
-
+const vqv = (nome, idade) => {
+  if (!nome || !idade) {
+    return undefined;
+  }
+  const abstract = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  return abstract;
+};
 module.exports = vqv;
