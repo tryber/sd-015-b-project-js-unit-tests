72e892a2e3857d966b5a43de7d399b41d621866f
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-01 19:00:40 -0300

    Cria function que retorna nome do Estudante e mensagem fixa.

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (valueEstudante) => {
+  const estudante = {
+    name: valueEstudante,
+    feedback: () => { return 'Eita pessoa boa!'}
+  }
+  return estudante;
+};
 
 module.exports = createStudent;
