8bb08ab455edf393bee49c78d016d09d6d434b26
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-01 17:57:52 -0300

    Altera implementaçao da parte 1.

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -31,7 +31,7 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // Teste se circle retorna um objeto.
     let fCircle = circle(30);
     let objeto = { radius: 30, area: 2826, circumference: 188.4 };
-    assert.notStrictEqual(fCircle, objeto);
+    assert.deepStrictEqual(fCircle, objeto);
     // Teste se o objeto retornado tem 3 entradas.
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
