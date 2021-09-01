2978b7ebf244b7bac45e9e88b3c86b7c46c3f540
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2021-09-01 15:40:04 -0300

    requesitos 2 e lint ok

--- src/average.js
+++ src/average.js
@@ -27,6 +27,7 @@ const average = (array) => {
     sum += array[i];
   }
   result = sum / array.length;
+    //  acessado https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary;
   return Math.round(result);
 };
 
