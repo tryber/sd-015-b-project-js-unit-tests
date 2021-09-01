3e615dacaf816e5c914e5bf426fb7e85a5327763
Rafael Toledo <rafael.toledo@engenharia.ufjf.br>
2021-09-01 15:21:15 -0300

    Requisito 1 feito

--- src/average.js
+++ src/average.js
@@ -14,19 +14,17 @@
 
 const average = (array) => {
   let soma = 0;
-  if(array.length === 0){
+  if (array.length === 0) {
     return undefined;
   }
-  for(let index = 0; index < array.length; index += 1){
-    if(typeof array[index] !== 'number'){
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
       return undefined;
     }
-    else{
-      soma = soma + array[index];
-    }
+    
+      soma += array[index];
   }
   return Math.round(soma / array.length);
 };
-
 
 module.exports = average;
