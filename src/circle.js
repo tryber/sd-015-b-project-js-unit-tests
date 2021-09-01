9a2a103bc5badd1347ccef5f3d941c4daf768888
aaguero96 <andre.luiz_1996@hotmail.com>
2021-09-01 15:13:25 -0300

    Fix lint part 4

--- src/circle.js
+++ src/circle.js
@@ -23,7 +23,7 @@ const circle = (radius) => {
   const PI = 3.14;
   if (!radius) { return undefined; }
   return {
-    radius: radius,
+    radius,
     area: parseFloat((PI * radius ** 2).toFixed(2)),
     circumference: parseFloat((2 * PI * radius).toFixed(2)),
   };
