ca953eba17a9461e11c583f3e5dd66f737020301
Antonio Souza <antoniofelipe2001@hotmail.com>
2021-09-01 14:46:23 -0300

    Requisito 1 finalizado

--- src/average.js
+++ src/average.js
@@ -13,10 +13,12 @@
 */
 
 const average = (array) => {
-  if (array.some(i => typeof (i) === 'string')) {
+  let arrayLength = array.length
+  if (array.some(i => typeof (i) === 'string') || arrayLength === 0) {
     return undefined
   }
-  return array.reduce((a, b) => a + b, 0)
+  const sum = array.reduce((a, b) => a + b, 0)
+  return Math.round(sum / arrayLength)
 };
 
 module.exports = average;
