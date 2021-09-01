ddd2c2e38bbdd6c968d5350f805fbe7676aad830
Joao Marco <joaomarco.matta@gmail.com>
2021-09-01 17:17:48 -0300

    Implementa requisito 5.

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const studentObj = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+
+  return studentObj;
+};
 
 module.exports = createStudent;
