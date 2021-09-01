aa9420b78290470dea2afd0b174fe89d288f368a
Gabriel Peralta <prates_peralta@hotmail.com>
2021-09-01 15:38:42 -0300

    finaliza o requisito 2

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -1,8 +1,8 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
-const assert = require('assert');
-const numbers = require('../src/numbers');
+const assert = require('assert");
+const numbers = require("../src/numbers");
 
 /*
   A função numbers recebe um array (tamanho variável) e retorna true se todos os parâmetros forem do tipo 'number' e false caso contrário.
@@ -16,12 +16,25 @@ const numbers = require('../src/numbers');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('2 - Implemente os casos de teste para a função `numbers`', () => {
-  it('Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros e falso caso contrário', () => {
-    assert.fail(); //apagar ou comentar essa linha
+describe("2 - Implemente os casos de teste para a função `numbers`", () => {
+  it("Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros e falso caso contrário", () => {
+    // assert.fail();
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+
+    const expected = numbers([1, 2, 3, 4, 5]);
+    assert.deepStrictEqual(expected, true);
+
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+    const expected2 = numbers([1, 2, '3', 4, 5]);
+    assert.deepStrictEqual(expected2, false);
+
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+
+    const expected3 = numbers([1, 'a', 3]);
+    assert.deepStrictEqual(expected3, false);
     // Escreva um teste em que a função recebe [' '] e retorna false
+
+    const expected4 = numbers([' ']);
+    assert.deepStrictEqual(expected4, false);
   });
 });
