928d4299392f237781e1300b037ed6692cb7777b
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-09-01 19:47:47 -0300

    Realiza requisito 9

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -43,16 +43,40 @@ Retorno:
 
 OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
+const characters = {
+  arya: {
+    name: 'Arya Stark',
+    class: 'Rogue',
+    phrases: ['Not today', 'A girl has no name.'],
+  },
+  brienne: {
+    name: 'Brienne Tarth',
+    class: 'Knight',
+    phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'],
+  },
+  melissandre: {
+    name: 'Melissandre',
+    class: 'Necromancer',
+    phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'],
+  },
+};
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(getCharacter(), undefined, 'ERROR: Empty array didnt return undefined')
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    assert.deepStrictEqual(getCharacter('Arya'), characters.arya, 'ERROR: Not expected input');
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    assert.deepStrictEqual(getCharacter('Brienne'), characters.brienne, 'ERROR: Not expected input');
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    assert.deepStrictEqual(getCharacter('Melissandre'), characters.melissandre, 'ERROR: Not expected input');
     // Teste se a função se os parâmetros não são Case Sensitive.
+    assert.deepStrictEqual(getCharacter('ArYa'), characters.arya, 'ERROR: Parameter not case sensitive');
+    assert.deepStrictEqual(getCharacter('BrIeNnE'), characters.brienne, 'ERROR: Parameter not case sensitive');
+    assert.deepStrictEqual(getCharacter('MeLiSsAnDrE'), characters.melissandre, 'ERROR: Parameter not case sensitive');
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    assert.strictEqual(getCharacter('Crazy Frog'), undefined, 'ERROR: Not existent parameter is returning something !== undefined')
   });
 });
