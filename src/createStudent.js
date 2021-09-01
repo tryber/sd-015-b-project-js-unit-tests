57666f52359b2b755184a2870474b1da9010084c
Walace <walacewab@gmail.com>
2021-09-01 20:32:03 -0300

    Adicionando requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const studentObject = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+
+  return studentObject;
+};
 
 module.exports = createStudent;
