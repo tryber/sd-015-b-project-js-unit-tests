dfbaa467943818ac5637d9b1a3a560997ae6a5b8
matheusg18 <matheusgabriel.18@gmail.com>
2021-09-01 18:12:42 -0300

    Requisito 10 -> Passo 5 do teste feito

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -96,6 +96,9 @@ describe('10 - Implemente os casos de teste e a função `createMenu`', () => {
     // objetoRetornado.order("coxinha");
     // objetoRetornado.consumption // Retorno: ["coxinha"]
     // ```
+    meuRestaurante.order('coxinha');
+    assert.strictEqual(meuRestaurante.consumption.includes('coxinha'), true);
+
     // Agora faça o PASSO 3 no arquivo `src/restaurant.js`.
     // --------------------------------------------------------------------------------------
     // TESTE 6: Verifique que as três orders seguintes, de bebidas e comidas mescladas, somam três itens no array `objetoRetornado.consumption` conforme os itens pedidos.
