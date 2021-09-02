408ff07bab271f62c635a94aacc0ba16083e3f0b
Samuel Pereira <gmr.quest.10@gmail.com>
2021-09-02 13:28:29 -0300

    Tenta corrigir o requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -20,9 +20,9 @@ const vqv = (nome, idade) => {
   if (!nome && !idade) return undefined;
   
   if (nome || idade) {
-    return `Oi, meu nome é ${nome}!
-    Tenho ${idade} anos,
-    trabalho na Trybe e mando muito em programação!
+    return `    Oi, meu nome é ${nome}! 
+    Tenho ${idade} anos, 
+    trabalho na Trybe e mando muito em programação! 
     #VQV!`;
   }
 };
