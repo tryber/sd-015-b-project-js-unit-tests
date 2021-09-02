1e8f39e4097117b6540c5258f4070f70e49fdbbe
Flavio Junior <flaviopds.jr@gmail.com>
2021-09-01 23:13:08 -0300

    req 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,20 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (typeof nome !== 'string') {
+    return undefined;
+  }
+  if (typeof idade !== 'number') {
+    return undefined;
+  }
+
+  const string = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  
+  return string;
+};
 
 module.exports = vqv;
