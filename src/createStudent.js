d877b820382677561c63edd5ffc0bb570b5e7266
Valdênio <valdeniomartinho@gmail.com>
2021-09-02 11:11:48 -0300

    adiciona requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,14 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const obj = {
+    name: name,
+    feedback:function(){
+      return 'Eita pessoa boa!'
+    }
+  }
+  return obj;
+};
 
 module.exports = createStudent;
