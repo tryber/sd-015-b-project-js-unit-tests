94bb4da32279e5e3b1c1f61c30c6109c24226f3f
Allan Melo <allan-m-silva@hotmail.com>
2021-09-01 23:57:34 -0300

    arruma requisito 1

--- src/average.js
+++ src/average.js
@@ -14,10 +14,17 @@
 
 const average = (array) => {
   let soma = 0;
-  for (let index = 0; index <= array.lenght; index += 1) {
+  if (array.length === 0) { 
+    return undefined; 
+  }
+  for (let index = 0; index < array.length; index += 1) { 
+    if (typeof (array[index]) !== 'number') {
+      return undefined;
+    }
     soma += array[index];
   }
-  return soma / array.lenght;
+
+  return Math.round(soma / array.length);
 };
 
 module.exports = average;
