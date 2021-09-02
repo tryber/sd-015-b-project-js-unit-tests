ebbeebe68b4c406f48bdf93ca5196b00513dfa0a
Wander Luiz <wander.srv@gmail.com>
2021-09-01 21:04:36 -0300

    Resoluçao do requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -15,12 +15,12 @@
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
 
-const myCounter = () => {
+const myCounter = (param1) => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+  for (var counter = 0; counter <= 1; counter += 1) {
+    myArray.push(param1[counter]);
+    for (var counter = 1; counter <= 11; counter += 1) {
+      myArray.push(param1[counter]);
     }
   }
   return myArray;
