41b8b487610dd6cac5cd87e9074fad203bf788e2
samuel <samuelsoutosilva14@gmail.com>
2021-09-02 10:23:15 -0300

    Adiciona requisito 4 - refatorado

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -36,7 +36,8 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     assert.strictEqual(circle(2).circumference, 4 * 3.14)
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.strictEqual(circle(3).circumference, 6 * 3.14)
+    assert.strictEqual(circle(3).area, 3.14 * 3 * 3)
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    assert.deepStrictEqual(circle(3), {radius: 3, area: (3.14 * 3 * 3), circumference: (6 * 3.14)})
   });
 });
