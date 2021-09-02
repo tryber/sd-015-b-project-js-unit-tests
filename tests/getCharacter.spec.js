ed8c179b012cbefc07f9d94a8b3765e3a0befd04
Hugoacr <hugoacr@yahoo.com.br>
2021-09-01 23:24:59 -0300

    responde requisito 9

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -46,13 +46,19 @@ OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enq
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(getCharacter(), undefined);
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    assert.strictEqual(getCharacter('Arya')['name'], 'Arya Stark');
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    assert.strictEqual(getCharacter('Brienne')['name'], 'Brienne Tarth');
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    assert.strictEqual(getCharacter('Melissandre')['name'], 'Melissandre');
     // Teste se a função se os parâmetros não são Case Sensitive.
+    assert.strictEqual(getCharacter('Arya') === getCharacter('ArYa'), true);
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    assert.strictEqual(getCharacter('Hugo'), undefined);
   });
 });
