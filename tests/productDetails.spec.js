9c0e194ed81d7bad8e70ded18e6cf31a43f191bc
Flavio Junior <flaviopds.jr@gmail.com>
2021-09-02 06:10:50 -0300

    req 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const { type } = require('os');
 const productDetails = require('../src/productDetails');
 
 /*
@@ -30,12 +31,22 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+    
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    const Arraytype = Array.isArray(productDetails('Alcool gel', 'Máscara'));
+    const pureArray = productDetails('Alcool gel', 'Máscara')
+
+    assert.strictEqual(Arraytype, true, 'Não é um array')
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails.length, 2, 'Não tem dois itens');
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof pureArray[0] && typeof pureArray[1], 'object', 'Não são Objetos');
     // Teste que os dois objetos são diferentes entre si.
+    assert.strictEqual(pureArray[0] !== pureArray[1], true, 'Os arrays não são iguais');
     // Teste que os dois productIds terminam com 123.
+    const string1 = pureArray[0].details.productId.substr(-3);
+    const string2 = pureArray[1].details.productId.substr(-3);
+    assert.strictEqual(string1 && string2, '123', 'Não são iguais');
   });
 });
