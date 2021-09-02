1919b90cde76f601eaa5fcc7383cb08f91f786de
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-02 15:32:08 -0300

    Refatora teste 1,2 e 3. Requisito 10

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -51,22 +51,28 @@ describe('10 - Implemente os casos de teste e a função `createMenu`', () => {
     // TESTE 1: Verifique se o retorno da função createMenu() é um objeto que possui, 
     // mas não é necessariamente é limitado à chave `fetchMenu`, a qual tem como valor uma função.
     // ```
-    // const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
-    assert.strictEqual(typeof createMenu(), 'object');
-    assert.strictEqual(typeof createMenu().fetchMenu, 'function');
+    const objetoRetornado1 = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
+    assert.strictEqual(typeof objetoRetornado1.fetchMenu, 'function');
     // ```
     // TESTE 2: Verifique que, dado que a função createMenu foi chamada com o objeto: `{ food: {}, drink: {} }`, 
     // verifique que 'objetoRetornado.fetchMenu()' retorna um objeto cujas chaves são somente `food` e `drink`.
     // ```
-    // const objetoRetornado = createMenu({ food: {}, drink: {} });
+    const objetoRetornado2 = createMenu({ food: {}, drink: {} });
     // objetoRetornado.fetchMenu() // Retorno: { food: {}, drink: {}}
-    assert.strictEqual(createMenu({ food: {}, drink: {} }.fetchMenu(), { food: {}, drink: {} }));
+    assert.deepStrictEqual(objetoRetornado2.fetchMenu(), {
+      food: {},
+      drink: {},
+    });
     // ```
     // TESTE 3: Verifique que o menu passado pra função createMenu é identico ao menu recuperado pela função 'objetoRetornado.fetchMenu'
     // ```
-    // const objetoRetornado = createMenu(objetoQualquer);
+    const objetoQualquer3 = {
+      name: 1,
+      segundo: 'bola',
+    };
+    const objetoRetornado3 = createMenu(objetoQualquer3);
     // objetoRetornado.fetchMenu() // Retorno: objetoQualquer
-    assert.strictEqual(createMenu({}).fetchMenu(), {});
+    assert.deepStrictEqual(objetoRetornado3.fetchMenu(), objetoQualquer3);
     // ```
     // Agora faça o PASSO 1 no arquivo `src/restaurant.js`.
     // --------------------------------------------------------------------------------------
