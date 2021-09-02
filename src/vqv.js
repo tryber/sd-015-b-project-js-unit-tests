ac37322780a295d3b0cd84c245022d9d1d86b3aa
Alex Menezes <alexmenezes.business@outlook.com>
2021-09-02 18:01:08 -0300

    adiciona requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
-
+const vqv = (nome, idade) => {
+  let resultado = 0;
+  if (nome === undefined || idade === undefined) {
+    resultado = undefined;
+    return resultado;
+  }
+ return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+};
 module.exports = vqv;
