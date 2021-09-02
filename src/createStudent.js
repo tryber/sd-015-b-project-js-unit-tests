8003448583a7b48e326cd9dbfe1700231f19e491
ArthurMoreira <arthurmcs96@gmail.com>
2021-09-01 22:58:19 -0300

    adiciona o requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,14 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  let obj = {
+    name: nome,
+    feedback() {
+      return 'Eita pessoa boa!';
+    },
+  };
+  return (obj);
+};
 
 module.exports = createStudent;
