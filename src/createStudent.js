f154432996ed5a6c22ceace4b71f7b2a0a932906
samuel <samuelsoutosilva14@gmail.com>
2021-09-02 10:35:02 -0300

    adiciona Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,10 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const object = { name };
+  object.feedback = () => 'Eita pessoa boa!';
+  return object;
+};
 
 module.exports = createStudent;
