564295eeb89f64830d5df4b7e98aa2967fddb7d9
Adriana <adrianam754@gmail.com>
2021-09-02 12:10:27 -0300

    Adicionada requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,11 +19,13 @@ const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (var numbers = 2; numbers <= 3; numbers += 1) {
+      myArray.push(numbers);
     }
   }
   return myArray;
 };
+
+console.log(myCounter());
 
 module.exports = myCounter;
