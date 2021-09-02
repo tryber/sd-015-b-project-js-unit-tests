e8f328d64b62ced11a6abafcbf49fdf3fd21c4e8
Denis Jonathan <denisjonathan7@gmail.com>
2021-09-02 16:51:03 -0300

    Requisito 10: Teste 8 refatorado

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -102,10 +102,11 @@ describe('10 - Implemente os casos de teste e a função `createMenu`', () => {
     // --------------------------------------------------------------------------------------
     // TESTE 8: Verifique que, ao chamar `objetoRetornado.pay()`, retorna-se a soma dos preços de tudo que foi pedido, conforme registrado em `objetoRetornado.consumption`
     // ```
-    // objetoRetornado.order('coxinha');
-    // objetoRetornado.order('agua');
-    // objetoRetornado.order('coxinha');
-    // objetoRetornado.pay() // Retorno: somaDosPreçosDosPedidos
+    const objetoRetornado8 = createMenu(barzinho);
+    objetoRetornado8.order('coxinha');
+    objetoRetornado8.order('agua');
+    objetoRetornado8.order('coxinha');
+    assert.strictEqual(objetoRetornado8.pay(), 12.87); // Retorno: 12.87
     // ```
     // Agora faça o PASSO 4 no arquivo `src/restaurant.js`.
   });
