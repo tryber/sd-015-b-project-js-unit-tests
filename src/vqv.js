12505901bd8337fada423a0b70bd7379195dd8da
Daniel Pordeus Gonzalez <danielpordeusgonzalez@hotmail.com>
2021-09-02 00:45:46 -0300

    comita 3 quesito

--- src/vqv.js
+++ src/vqv.js
@@ -15,7 +15,18 @@
       trabalho na Trybe e mando muito em programação!
       #VQV!'
 */
-
-const vqv = (nome, idade) => {};
+// após fica travado no exercicio visitei o github de Paulo Flóra https://github.com/tryber/sd-015-b-project-js-unit-tests/pull/36/commits/01884a50403c94a7d5c2acba92ca34b31663fb29 
+// afim de entender o requesito e após entender como o resultado foi feito
+// e após 3 tentativas sem olhar ou consultar consegui entender o requesito
+const vqv = (nome, idade) => {
+  if (typeof nome !== 'string') {
+    return undefined;
+  }
+  const presentation = `Oi, meu nome é ${nome}!\n`
+  + `Tenho ${idade} anos,\n`
+  + 'trabalho na Trybe e mando muito em programação!\n'
+  + '#VQV!';
+  return presentation;
+};
 
 module.exports = vqv;
