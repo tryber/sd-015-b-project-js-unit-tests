8f4d655a2e57231e5c52200f3c12e422e263b275
jpcp0614 <jpcp0614@gmail.com>
2021-09-02 22:29:28 -0300

    const name e function feedback

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const name = { name: nome, feedback: () => 'Eita pessoa boa!' };
+  return name;
+};
 
 module.exports = createStudent;
