c5621712f80c9358bf09b2eb4715fad7437eaeb2
Miguel_Reis <guelsouzar@gmail.com>
2021-09-03 14:12:47 -0300

    corrigindo o average

--- src/average.js
+++ src/average.js
@@ -15,10 +15,10 @@
 // consultei o repositorio do André Luiz S. pois estava tento dificuldades pois estava com dificuldade no typeof e no Math.Round
 // https://github.com/tryber/sd-015-b-project-js-unit-tests/commit/e6b6190926b6efbc188394c111c3ffa451f8d432
 const average = (array) => {
-  if (typeof array === 'undefined') { return undefined; } 
+  if (typeof array[0] === 'undefined') { return undefined; } 
   let results = 0;
   let soma = 0;
-  for (let i = 0; i < array.length; i = +1) {
+  for (let i = 0; i < array.length; i += 1) {
       if (typeof array[i] !== 'number') { return undefined; }
       
       soma += array[i];
@@ -28,5 +28,4 @@ const average = (array) => {
   return Math.round(results);
 };
 
-console.log(average([]));
 module.exports = average;
