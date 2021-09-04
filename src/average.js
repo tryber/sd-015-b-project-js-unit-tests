2e2627e6b202fc8d1dfccb60a2e6b51462eb79fb
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-04 01:09:38 -0300

    Requisito 1 - Cria uma funçao que verifica se todos os elementos de um array sao do tipo number e retorna um valor booleano // Cria uma funçao que retorna undefined caso um dos itens do array nao seja number ou o length do array seja zero e retorna a media arredondada (< 0.5, para baixo ou > 0.5 para cima) dos itens do array passado

--- src/average.js
+++ src/average.js
@@ -1,8 +1,7 @@
 /*
   A função average recebe um array (tamanho variável) e retorna a média dos valores recebidos.
   Caso a função receba algum valor não númerico ou um array vazio,
-  o valor undefined deve ser retornado.
-  Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
+  o valor undefined deve ser retornado. Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
 
   Parâmetros:
     - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
@@ -12,8 +11,33 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
+/**
+ * todo criar as primeiras condições: retornar undefined para arr.length === 0 e se houver algum valor nao numerio no array
+ */
+
+const isAllNumber = (arr) => {
+  let isNumber = true;
+  for (item of arr) {
+    if (typeof item !== 'number') {
+      isNumber = false;
+    }
+  }
+  return isNumber;
+};
+
+const average = (arr) => {
   // add your implementation here
+  if (isAllNumber(arr) === false || arr.length === 0) {
+    return undefined;
+  } else {
+    let sum = 0;
+    for (number of arr) {
+      sum += number;
+    }
+    const meanInt = Math.round(sum / arr.length);
+
+    return meanInt;
+  }
 };
 
 module.exports = average;
