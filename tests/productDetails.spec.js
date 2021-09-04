7edb8cdca363f1cc939e81ec9545fac1d59332ad
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2021-09-04 13:36:21 -0300

    requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -34,9 +34,14 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste que o retorno da função é um array.
   assert.deepStrictEqual( Array.isArray(productDetails()), true)
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.deepStrictEqual( productDetails().length, 2)
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof productDetails('firstProduct', 'secondProduct')[0], 'object')
+    assert.deepStrictEqual(typeof productDetails('firstProduct', 'secondProduct')[0], 'object')
     // Teste que os dois objetos são diferentes entre si.
+    assert.strictEqual((productDetails('a', 'b')[0] === productDetails('a', 'b')[1]), false)
     // Teste que os dois productIds terminam com 123.
-    assert.strictEqual(productDetails('firstProduct', 'secondProduct').details.productId.includes(123), true)
+    assert.strictEqual(productDetails('a', 'b')[0].details.productId.includes(123), true)
+    assert.strictEqual(productDetails('a', 'b')[1].details.productId.includes(123), true)
   });
 });
