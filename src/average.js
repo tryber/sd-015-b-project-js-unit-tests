e654e1c169a30ac0fcea7f125e8d65e8f2b883c7
raphael <raphabarzotto@gmail.com>
2021-09-04 23:30:32 -0300

    entrega requisito 1

--- src/average.js
+++ src/average.js
@@ -12,8 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // para fazer o commit inicial
+const average = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
+
+  let sum = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    sum += array[index];
+  }
+  return Math.round(sum / array.length);
 };
 
 module.exports = average;
