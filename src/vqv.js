6274148b077a8c3992fbdb7f8d3f31947fc597e6
André E. Souto <andreesouto@hotmail.com>
2021-09-05 20:42:35 -0300

    adicionou requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,14 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (typeof nome !== 'string' || typeof idade !== 'number') {
+    return undefined;
+  }
+  let frase = `Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`;
+};
 
 module.exports = vqv;
