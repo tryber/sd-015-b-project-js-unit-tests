bd56f76d098c0457b9fafa11df3ca7aa914d92a8
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-09-05 10:17:53 -0300

    Adiciona requisito 9, 2/6

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -46,10 +46,12 @@ OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enq
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.deepStrictEqual(getCharacter(), undefined);
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    assert.deepStrictEqual(getCharacter('Arya'),  {name: 'Arya Stark', class: 'Rogue', phrases: ['Not today', 'A girl has no name.'],} );
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
     // Teste se a função se os parâmetros não são Case Sensitive.
