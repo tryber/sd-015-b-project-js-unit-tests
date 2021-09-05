4e7d0b2b85cb7bb8640597ab3915a4c648e161a5
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-09-05 10:29:51 -0300

    Adiciona requisito 9, 4/6

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -53,8 +53,11 @@ describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
     assert.deepStrictEqual(getCharacter('Arya'),  {name: 'Arya Stark', class: 'Rogue', phrases: ['Not today', 'A girl has no name.'],} );
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    assert.deepStrictEqual(getCharacter('Brienne'),  { name: 'Brienne Tarth', class: 'Knight', phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'] });
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    assert.deepStrictEqual(getCharacter('Melissandre'), { name: 'Melissandre', class: 'Necromancer', phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'] });
     // Teste se a função se os parâmetros não são Case Sensitive.
+
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
   });
 });
