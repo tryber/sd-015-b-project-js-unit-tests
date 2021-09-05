5783dfe340471b6d84ba6719fef89b09c065ad0b
André E. Souto <andreesouto@hotmail.com>
2021-09-05 20:29:09 -0300

    tentando resolver requisito 1

--- src/average.js
+++ src/average.js
@@ -13,10 +13,13 @@
 */
 
 const average = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
   let final = 0;
   for (let i = 0; i < array.length; i += 1) {
   if (typeof array[i] !== 'number') {
-    return 'undefined';
+    return undefined;
   } 
   }
   return Math.round(final / array.length);
