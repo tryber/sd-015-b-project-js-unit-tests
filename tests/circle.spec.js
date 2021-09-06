10dfa793fe5c9f74908779ccfd745fa7d4e01522
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2021-09-06 15:41:45 -0300

    Termina requisito 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -33,8 +33,12 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-    assert.strictEqual(circle(2).circumference, 12.56)
+    assert.strictEqual(circle(2).circumference, 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    const realArea = parseFloat(circle(3).area.toPrecision(4));
+    assert.deepStrictEqual(realArea, 28.26);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    const circle3 = { radius: 3, area: 28.259999999999998, circumference: 18.84 };
+    assert.deepStrictEqual(circle(3), circle3);
   });
 });
