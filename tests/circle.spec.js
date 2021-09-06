3c31f0e182e023957a368ed5e5d1947defb96caf
douglas <douglasmonteiro.m@gmail.com>
2021-09-06 11:06:38 -0300

    Adiciona equisito 5 e refatora requisito 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -38,8 +38,6 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     assert.ok(Object.values(circle(3))[1] === 28);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    assert.ok(Object.values(circle(3))[0] === 3);
-    assert.ok(Object.values(circle(3))[1] === 28);
-    assert.ok(Object.values(circle(3))[2] === 19);
+    assert.deepStrictEqual(circle(3), { radius: 3, area: 28, circumference: 19 });
   });
 });
