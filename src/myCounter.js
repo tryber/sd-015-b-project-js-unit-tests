406488191f79b7aff9c409f4175485a77f436647
PedroAugmed <pedroaugmed7@gmail.com>
2021-09-07 13:36:38 -0300

    ADD Requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -1,8 +1,3 @@
-/* eslint-disable no-var */
-/* eslint-disable block-scoped-var */
-/* eslint-disable no-redeclare */
-/* eslint-disable vars-on-top */
-
 /*
   A função myCounter possui dois loops aninhados que inserem valores dentro de um array.
   Como podemos perceber, eles vão adicionando valores ao array até sua condição de parada.
@@ -16,14 +11,15 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  const myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let i = 2; i <= 3; i += 1) {
+      myArray.push(i);
     }
   }
   return myArray;
 };
 
 module.exports = myCounter;
+console.log(myCounter());
