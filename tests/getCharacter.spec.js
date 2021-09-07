0982353df244eaf5276f00a68386ba5796540607
Marina Marques <marina.marqueso@outlook.com>
2021-09-07 11:09:27 -0300

    Adiciona requisito 9

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-unused-vars */
 
 const assert = require('assert');
+const { get } = require('http');
 const getCharacter = require('../src/getCharacter');
 
 /*
@@ -46,13 +47,30 @@ OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enq
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(getCharacter(), undefined);
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    let aryaObj = {name: 'Arya Stark',
+      class: 'Rogue',
+      phrases: ['Not today', 'A girl has no name.'],
+    };
+    assert.deepStrictEqual(getCharacter('Arya'), aryaObj);
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    let brienneKeys = {name: 'Brienne Tarth',
+      class: 'Knight',
+      phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'],
+    };
+    assert.deepStrictEqual(getCharacter('Brienne'), brienneKeys);
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
-    // Teste se a função se os parâmetros não são Case Sensitive.
+    let melissandreKeys = {name: 'Melissandre',
+      class: 'Necromancer',
+      phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'],
+    };
+    assert.deepStrictEqual(getCharacter('Melissandre'), melissandreKeys);
+    // Teste se a função se os parâmetros não são Case Sensitive. (tem que ser iguais para dar, já que Arya === arya === aRYa === aryA)
+    assert.strictEqual(getCharacter('Arya') !== getCharacter('arya'), false);
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    assert.strictEqual(getCharacter('dsadsa'), undefined);
   });
 });
