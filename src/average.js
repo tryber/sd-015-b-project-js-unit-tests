efb81964bf9451370659e572ea8571aba81cda37
BrenoRCBrito <brenorcbrito@gmail.com>
2021-09-07 18:23:12 -0300

    corrige average.js

--- src/average.js
+++ src/average.js
@@ -14,13 +14,18 @@
 
 const average = (array) => {
   let sum = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
   for (let i = 0; i < array.length; i += 1) {
     if (typeof (array[i]) !== 'number') {
       return undefined;
     }
     sum += Math.round(array[i]);
   }
-  return sum / array.length;
+  return Math.round(sum / array.length);
 };
+
+average([]);
 
 module.exports = average;
