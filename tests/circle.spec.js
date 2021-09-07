26d9445b4e30061394343ebc8bcf03fa1bd022d2
Daniel Pordeus Gonzalez <danielpordeusgonzalez@hotmail.com>
2021-09-07 01:14:51 -0300

    comita o exercici4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-unused-vars */
 
 const assert = require('assert');
+const { type } = require('os');
 const circle = require('../src/circle');
 
 /*
@@ -16,6 +17,7 @@ const circle = require('../src/circle');
     - circle(7) // Retorno: {radius: 7, area: 153.86, circumference: 43.96}
     - circle(3) // Retorno: {radius: 3, area: 28,26, circumference: 18.84}
 
+    +
   DICA: Números de ponto flutuante em JavaScript são imprecisos!  Para testar, vá no seu navegador e faça `0.2 + 0.1`.
         Uma solução pra isso pode ser fazer a soma no seguinte formato: `parseFloat((0.2 + 0.1).toPrecision(2))`.
         Use esse conhecimento para te ajudar a lidar com possíveis problemas que esses testes trarão!
@@ -25,13 +27,30 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    assert.fail();
+    
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
-    // Teste se o objeto retornado tem 3 entradas.
+    assert.strictEqual(typeof circle(1), 'object')
+    // Teste se o objeto retornado tem 3 entradas. 
+    const Circle = circle(1);
+    const test = Object.entries(Circle).length
+    assert.strictEqual(test, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    let raio = circle(2)
+    let expected = Object.values(raio)[2];
+    assert.strictEqual(expected, 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    let raio3 = circle(3)
+    expected = Object.values(raio3)[1];
+    let Result = parseFloat((expected).toPrecision(4))
+    assert.strictEqual(Result, 28.26);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    const testResult = circle(3)
+    let roundedArea = parseFloat((testResult.area).toPrecision(4))
+    testResult.area = roundedArea
+    expected = {radius: 3, area: 28.26, circumference: 18.84}
+    assert.deepStrictEqual(testResult, expected)
   });
 });
