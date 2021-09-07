d714d3286b5c0ce454d50642f3a4eedbffcacebf
Flávia Labanca <flavialabanca@gmail.com>
2021-09-06 22:18:30 -0300

    Entrega final do  projeto com ajuda da Camilla del Guerso, Daniellen Cestari, Gabriel Alves e Leandro Oliveira.

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -64,7 +64,7 @@ describe('10 - Implemente os casos de teste e a função `createMenu`', () => {
     
     // TESTE 2: Verifique que, dado que a função createMenu foi chamada com o objeto: `{ food: {}, drink: {} }`, 
     // verifique que 'objetoRetornado.fetchMenu()' retorna um objeto cujas chaves são somente `food` e `drink`.
-    // ```
+    
     const objetoRetornado = createMenu({ food: {}, drink: {} });
     assert.strictEqual(typeof objetoRetornado.fetchMenu(), 'object');
     assert.deepStrictEqual(Object.keys(objetoRetornado.fetchMenu()), ['food', 'drink']);
@@ -132,8 +132,9 @@ describe('10 - Implemente os casos de teste e a função `createMenu`', () => {
     // objetoRetornado.order('agua');
     // objetoRetornado.order('coxinha');
     // objetoRetornado.pay() // Retorno: somaDosPreçosDosPedidos
+    
     assert.strictEqual(buteco.pay(), 12.87);
-    // ```
+    
     // Agora faça o PASSO 4 no arquivo `src/restaurant.js`.
   });
 });
