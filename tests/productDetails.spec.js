85806b894beb21442a6e78ee116b135b70483b01
Miguel_Reis <guelsouzar@gmail.com>
2021-09-07 05:09:34 -0300

    requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -28,14 +28,24 @@ const productDetails = require('../src/productDetails');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
+// estava com dificuldade no ultimo requisito então consultei o repositorio do André Luiz S.
+// https://github.com/tryber/sd-015-b-project-js-unit-tests/pull/102/commits/9205ebd68d65da5d8e338ab37eb7b6d9bf217396
+
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.ok(Array.isArray(productDetails('1', '2')), 'array');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.ok(productDetails('1', '2'), 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof productDetails('1', '2'), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.ok(productDetails(1) !== productDetails(2));
     // Teste que os dois productIds terminam com 123.
+    const protudo = productDetails('1', '2');
+    assert.strictEqual(protudo[0].details.productId.endsWith('123'), true);
+    assert.strictEqual(protudo[1].details.productId.endsWith('123'), true);
   });
 });
