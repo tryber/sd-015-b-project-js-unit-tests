d7f515147aa9e4d72a750c4937e15974bd54e7cd
Yamamoto Andre <andre.yamamoto07@gmail.com>
2021-09-07 04:55:30 -0300

    implementaçao no objCalculatosr.js versao 3

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -22,7 +22,7 @@
 const calculator = {
   add: (num, num2) => num + num2,
   mult: (num, num2) => num * num2,
-  div: (num,num2) => num / num2,
+  div: (num,num2) => Math.floor(num / num2),
   sub: (num, num2) => num - num2,
 };
 
