590093bb0a1ab8483c67ace8615b4f23ccee7948
Jeffrey <jeffreydouglascsantos@gmail.com>
2021-09-08 03:40:59 -0300

    requisito 3 e 9

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,12 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  let hello = `Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`;
+  return hello;
+};
 
 module.exports = vqv;
