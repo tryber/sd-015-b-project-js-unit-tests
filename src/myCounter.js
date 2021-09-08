3e75ae92d93c5090b23157c01810331678d24e15
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-07 21:08:49 -0300

    Requisito 8 - Conserta a funçao myCounter

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,14 +16,21 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  const myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
+    for (let counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }
   return myArray;
 };
+
+/**
+ * ! Não se pode usar o var, pois ele é 'funciton scoped', ou seja, os dois loops for tem acesso ao valor var. Dessa forma os loops seriam: counter = 0 / counter = 2 --> quando voltar para o primeiro for, o counter será igual a 2.
+ * * Já com o let, que é 'block scoped', os valores de counter sao contidos nos loops for e, por isso, atuam de forma independente.
+ */
+
+console.log(myCounter());
 
 module.exports = myCounter;
