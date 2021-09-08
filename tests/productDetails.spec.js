15effa86b9c6c0e720c40dae76ad5b3163e05681
BrenoRCBrito <brenorcbrito@gmail.com>
2021-09-07 23:16:24 -0300

    adiciona productDetails.js

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,19 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+    const test = productDetails('luva', 'bota');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(test), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(test.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof test[0] === 'object', true);
+    assert.strictEqual(typeof test[1] === 'object', true);
     // Teste que os dois objetos são diferentes entre si.
+    assert.strictEqual(test[0] === test[1], false);
     // Teste que os dois productIds terminam com 123.
+    assert.strictEqual(test[0].details.productId.match(/\d+/g).map(Number)[0] === 123, true);
+    assert.strictEqual(test[1].details.productId.match(/\d+/g).map(Number)[0] === 123, true);
   });
 });
