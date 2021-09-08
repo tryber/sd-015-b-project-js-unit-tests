7bd42f762c9bf0a2613af370f63efb2b3e3dcb44
fariasao <enzofarias2010@gmail.com>
2021-09-07 22:00:09 -0300

    requisito 4 e refatoracao

--- src/vqv.js
+++ src/vqv.js
@@ -18,10 +18,10 @@
 
 const vqv = (nome, idade) => {
   if (!nome && !idade) return undefined;
-  return `Oi, meu nome é ${nome}! 
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+  return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
 
 module.exports = vqv;
