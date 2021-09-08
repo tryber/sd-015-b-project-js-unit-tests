7242fcdea192ac4362e09f8ed58b57fa55b73080
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-07 21:12:34 -0300

    Requisito 9 - Teste 1: Verifica se a funçao retorna undefined se nenhum parametro for definido

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
+const { strict } = require('assert');
 const assert = require('assert');
 const getCharacter = require('../src/getCharacter');
 
@@ -46,9 +47,9 @@ OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enq
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(undefined, getCharacter());
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
