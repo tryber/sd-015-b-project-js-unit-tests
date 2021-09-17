461a6d392389fef5cc06e215093e94f0eb462d80
Jamez Mayke <jamezmayke@gmail.com>
2021-09-17 15:36:14 -0300

    ajustes do lint requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -16,13 +16,12 @@
 */
 
 const createStudent = (name) => {
-
   return {
     name: name,
-    feedback: () => {
-      return `Eita pessoa boa!`
+    feedback: function() {
+      return 'Eita pessoa boa!';
     }
-  }
+  };
 };
 
 module.exports = createStudent;
