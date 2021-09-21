ffdd1d99c487780086f1f32ff28ade149e73c89a
taiserrao <contato.taianeserrao@gmail.com>
2021-09-21 16:08:23 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nameStudent) => {
+  let studentFeedback = {
+    name: nameStudent,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return studentFeedback;
+};
 
 module.exports = createStudent;
